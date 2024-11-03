aacf05ff9cea9957442b747cc838356db44e2f58
Milind Rai <milindrai365@gmail.com>
2024-11-03 13:25:29 +0530

    Add S3 bucket configuration and EC2 instance for project deployment

--- main.tf
+++ main.tf
@@ -30,27 +30,28 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "public-read"
 }
 
+# Upload files to S3 bucket
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
-  key = "index.html"
+  key    = "index.html"
   source = "index.html"
-  acl = "public-read"
+  acl    = "public-read"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.mybucket.id
-  key = "error.html"
+  key    = "error.html"
   source = "error.html"
-  acl = "public-read"
+  acl    = "public-read"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "profile" {
   bucket = aws_s3_bucket.mybucket.id
-  key = "profile.png"
+  key    = "profile.png"
   source = "profile.png"
-  acl = "public-read"
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -63,14 +64,99 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  depends_on = [ aws_s3_bucket_acl.example ]
+  depends_on = [aws_s3_bucket_acl.example]
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-00f251754ac5da7f0" 
-  instance_type = "t2.micro" 
+# Define IAM policy allowing S3 access
+resource "aws_iam_policy" "s3_access" {
+  name        = "S3AccessPolicy"
+  description = "Policy to allow access to S3 bucket"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject"],
+        Resource = "arn:aws:s3:::${aws_s3_bucket.mybucket.id}/*"
+      }
+    ]
+  })
+}
+
+# Define IAM role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name               = "EC2Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the IAM policy to the role
+resource "aws_iam_role_policy_attachment" "s3_access_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access.arn
+}
+
+# Define IAM instance profile for EC2
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2InstanceProfile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# Define the EC2 instance with user data to copy files from S3
+resource "aws_instance" "web_server" {
+  ami                    = "ami-00f251754ac5da7f0"
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y aws-cli httpd
+
+    # Start the Apache web server
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Copy files from S3
+    aws s3 cp s3://${aws_s3_bucket.mybucket.bucket}/index.html /var/www/html/index.html
+    aws s3 cp s3://${aws_s3_bucket.mybucket.bucket}/error.html /var/www/html/error.html
+
+    # Restart Apache to load new files
+    systemctl restart httpd
+  EOF
 
   tags = {
     Name = "EC2 Instance for the project"
+  }
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
